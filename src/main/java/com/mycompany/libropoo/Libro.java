c4c3e78da0101f1fc8850e901b1ddd3e0ba5d9ff
Nohe09 <nohelly.patinoc@konradlorenz.edu.co>
2024-04-22 22:02:12 -0500

    upadate

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,56 @@
 package com.mycompany.libropoo;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacion; 
+    private String genero;
+    private boolean leido;
+
+    public Libro(String titulo, String autor, int anoPublicacion, boolean leido,String genero) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacion = anoPublicacion;
+        this.leido = leido;
+        this.genero= genero;
+    }
+
+   
+    /**
+     * Complejidad temporal: O(1) - Tiempo constante.
+     * @return 
+     */
+    public String mostrarInformacion() {
+        return ( "Título: " + titulo + ", Autor: " + autor + ", Año de publicación: " 
+               + anoPublicacion+ ", Leído: " + (leido? "Sí" : "No"));
+       
+    }
+    
+    /**
+     * Complejidad temporal: O(1) - Tiempo constante.
+     */
+    public void marcarLeido() {
+        this.leido= true;
+    }
+    
+    /**
+     * Complejidad temporal: O(1) - Tiempo constante.
+     * @return 
+     */
+    public boolean getLeido(){
+        return leido;
+    }
+    
+    /**
+     * Complejidad temporal: O(1) - Tiempo constante.
+     * @return 
+     */
+    public boolean esAntiguo() {
+        if (anoPublicacion>=1974){
+            return false;
+        } else {
+            return true;
+        }
+    }
     
 }
