b10257ff53b5af3a598b6eb2915039d489cd90de
Nohe09 <nohelly.patinoc@konradlorenz.edu.co>
2024-04-22 22:03:03 -0500

    comentario de complejidad

--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,34 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
 public class Principal {
-    
+     /**
+    * Complejidad lineal: O(N) - Tiempo lineal.
+     * @param args
+    */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el título del libro:");
+        String titulo = sc.nextLine();
+        
+        System.out.println("Ingrese el genero del libro:");
+        String genero = sc.nextLine();
+
+        System.out.println("Ingrese el autor del libro:");
+        String autor = sc.nextLine();
+
+        System.out.println("Ingrese el año de publicación del libro:");
+        int anoPublicacion = sc.nextInt();
+      
+       Libro libro = new Libro (titulo,autor,anoPublicacion,genero);
+               libro.marcarLeido();
+
+        System.out.println(libro.mostrarInformacion());
+        System.out.println("¿El libro ha sido leído? " + (libro.getLeido() ? "Sí" : "No"));
+        System.out.println("¿El libro es antiguo? " + (libro.esAntiguo() ? "Sí" : "No"));
+
+        sc.close();
+    }
 }
